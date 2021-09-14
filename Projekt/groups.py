4c6c452960a8fa7f1639b8da19978d4e12a37974
Andrea Hrelja <codecommit-ahrelja-at-112137832195>
2021-09-14 22:47:52 +0200

    Initial commit

---
+++ Projekt/groups.py
@@ -0,0 +1,36 @@
+PRIRODOSLOVNI_PREDMETI = [
+    "Hrvatski jezik",
+    "Biologija",
+    "Geografija",
+    "Povijest",
+    "Priroda"
+]
+
+ZNANSTVENI_PREDMETI = [
+    "Fizika",
+    "Matematika",
+    "Kemija"
+]
+
+JEZICNI_PREDMETI = [
+    "Engleski jezik",
+    "Talijanski jezik (izborni)"
+]
+
+UMJETNICKI_PREDMETI = [
+    "Glazbena kultura"
+    "Likovna kultura"
+]
+
+STRUCNI_PREDMETI = [
+    "Informatika",
+    "Tehnička kultura"
+]
+
+SPORTSKI_PREDMETI = [
+    "Tjelesna i zdravstvena kultura"    
+]
+
+ETICKI_PREDMETI = [
+    "Vjeronauk - Katolički (izborni)"    
+]
